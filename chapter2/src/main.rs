0c02d9519ba0893268a495a041e9cea47fd9dc2d
DESKTOP-PCA6VUI\82105 <ttttwww10@gmail.com>
2023-01-10 14:27:43 +0900

    chapter2 finish

--- chapter2/src/main.rs
+++ chapter2/src/main.rs
@@ -11,22 +11,34 @@ fn main() {
 
     println!("secret num is {secret_number}");
 
-    println!("input your guess");
+    loop{
+        println!("input your guess");
 
-    // let 으로 변수 선언
-    // rust는 기본이 immutable로 상수임.
-    let mut guess = String::new();
+        // let 으로 변수 선언
+        // rust는 기본이 immutable로 상수임.
+        let mut guess = String::new();
 
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("Fail 2 read line");
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Fail 2 read line");
 
-    println!("you guess : {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                // println!("{}",_);
+                continue;
+            },
+        };
+        
+        println!("you guess : {guess}");
 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("too small!"),
-        Ordering::Greater => println!("too big!"),
-        Ordering::Equal => println!("perpect"),
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big!"),
+            Ordering::Equal => {
+                println!("perpect");
+                break;
+            }
+        }
     }
-
 }
