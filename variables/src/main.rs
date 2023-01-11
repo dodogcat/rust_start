c293e50385410dd01a42ad7fefca565f2e01b717
dodogcat <ttttwww10@gmail.com>
2023-01-11 17:25:23 +0900

    chapter3

---
+++ variables/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x = 5;
+    println!("value of x :{x}");
+    x = 6;
+    println!("value of x :{x}");
+}
---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+E:\jang\rust_start\variables\target\debug\deps\variables.exe: src\main.rs
+
+E:\jang\rust_start\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
